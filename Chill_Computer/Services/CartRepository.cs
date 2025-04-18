14f34be6c56f4cab615a32c4899f3e9a24d68bad
Bao1404 <lethebao1404@gmail.com>
2025-04-18 13:24:35 +0700

    commit cart

---
+++ Chill_Computer/Services/CartRepository.cs
@@ -0,0 +1,13 @@
+﻿using Chill_Computer.Models;
+
+namespace Chill_Computer.Services
+{
+    public class CartRepository
+    {
+        private readonly ChillComputerContext _context;
+        public CartRepository(ChillComputerContext context)
+        {
+            _context = context;
+        }
+    }
+}
